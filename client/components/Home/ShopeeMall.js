1e987b054e6d5619515e935b45c8b4e7f45e1def
huy.nq <huyquy2812@gmail.com>
2022-11-12 22:41:41 +0700

    add mall component (50%)

--- client/components/Home/ShopeeMall.js
+++ client/components/Home/ShopeeMall.js
@@ -5,6 +5,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
 
 export default function ShopeeMall() {
     return (
@@ -13,24 +15,36 @@ export default function ShopeeMall() {
                 <Image src={images.simple_banner[1]} alt="banner" />
             </div>
             <div className={styles.mall}>
-                <div className={styles.content}>
-                    <div className={styles.header}>
-                        <div>
-                            <Link href="#" legacyBehavior>
-                                <a className={styles.title}>Shopee mall</a>
-                            </Link>
-                            <div>
-                                
+                <div className={styles.header}>
+                    <div className={styles.header_left}>
+                        <Link href="#" legacyBehavior>
+                            <a className={styles.title}>Shopee mall</a>
+                        </Link>
+                        <div className={styles.items}>
+                            <div className={styles.item}>
+                                <Image src={images.mall_icons[0]} alt="mall-icon" className={styles.icon} />
+                                <span>7 Ngày Miễn Phí Trả Hàng</span>
+                            </div>
+                            <div className={styles.item}>
+                                <Image src={images.mall_icons[1]} alt="mall-icon" className={styles.icon} />
+                                <span>Hàng Chính Hãng 100%</span>
+                            </div>
+                            <div className={styles.item}>
+                                <Image src={images.mall_icons[2]} alt="mall-icon" className={styles.icon} />
+                                <span>Miễn Phí Vận Chuyển</span>
                             </div>
                         </div>
-                        <Link href="#" legacyBehavior>
-                            <a className={styles.show_all}>
-                                <span>Xem tất cả</span>
-                                <FontAwesomeIcon icon={faAngleRight} className={styles.show_all_icon} />
-                            </a>
-                        </Link>
                     </div>
-                    <div className={styles.content}></div>
+                    <Link href="#" legacyBehavior>
+                        <a className={styles.show_all}>
+                            <span>Xem tất cả</span>
+                            <FontAwesomeIcon icon={faAngleRight} className={styles.show_all_icon} />
+                        </a>
+                    </Link>
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.content_left}></div>
+                    <div className={styles.content_right}></div>
                 </div>
             </div>
         </div>
