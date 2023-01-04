e12fc0f1839929f5b9279acf919c4416d6845501
huy.nq <huyquy2812@gmail.com>
2023-01-04 20:24:09 +0700

    fix index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(helmet());
 //static file
 app.use(express.static(path.join(__dirname, "resources")));
 app.get("/", (req, res) => {
-    res.sendFile("index.html", { root: path.join(__dirname, "resources") });
+    res.sendFile("index.html", { root: path.join(__dirname, "src/resources") });
 });
 
 // add body-parser
